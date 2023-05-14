dc49e00654777df0f2e2addd810b19f446eb5453
lumpenop <swk5373@gmail.com>
2023-05-15 01:35:33 +0900

    [Etc] foundation setting react-query / react-navigation / jotai / emotion / 절대경로 / svg 설정

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Main from 'src/screens/main';
+
+const App = () => {
+  return (
+    <View>
+      <Main />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default App;
