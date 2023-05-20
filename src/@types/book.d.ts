7d820841f498744271f7756255e6b543ac18f0fb
wogha95 <wogha95@naver.com>
2023-05-21 00:44:45 +0900

    [Feat] BookItem 타입 작성

---
+++ src/@types/book.d.ts
@@ -0,0 +1,11 @@
+import { ImageSourcePropType } from 'react-native/types';
+
+interface IBookItem {
+  id: number;
+  thumbnailImage: ImageSourcePropType;
+  title: string;
+  price: number;
+  wishCount: number;
+  isSoldOut: boolean;
+  createdAt: string;
+}
