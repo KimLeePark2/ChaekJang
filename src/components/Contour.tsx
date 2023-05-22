1ae4b932bc18b9b1816717772fd89bdb23d63342
lumpenop <swk5373@gmail.com>
2023-05-22 22:33:23 +0900

    [Etc] 구분선

---
+++ src/components/Contour.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Contour = () => {
+  return (
+    <View style={{ height: 1, width: '100%', backgroundColor: '#777777' }} />
+  );
+};
+
+export default Contour;
