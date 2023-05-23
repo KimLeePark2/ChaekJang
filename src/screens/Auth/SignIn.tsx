313cde0d4030f031df8062881fde05f1d4b6a189
suu <suu0321@naver.com>
2023-05-24 00:31:39 +0900

    [Feat] 카카오로그인 연동
    내가 이겼다...

--- src/screens/Auth/SignIn.tsx
+++ src/screens/Auth/SignIn.tsx
@@ -1,18 +1,29 @@
+import { login } from '@react-native-seoul/kakao-login';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react';
-import { StyleSheet, View, Text, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button } from 'react-native';
 
 type PropsType = NativeStackScreenProps<RootStackParamsType, 'SignIn'>;
 
-const SignIn: React.FC<PropsType> = ({ navigation }) => {
-  const onPressBack = () => {
-    navigation.goBack();
-  };
-  return <View></View>;
-};
+const SignIn: React.FC<PropsType> = ({}) => {
+  const [result, setResult] = useState<string>('');
 
-const styles = StyleSheet.create({
-  block: {},
-});
+  const signInWithKakao = async (): Promise<void> => {
+    try {
+      const token = await login();
+      setResult(JSON.stringify(token));
+    } catch (err) {
+      console.error('login err', err);
+    }
+  };
+
+  console.log(result);
+
+  return (
+    <View>
+      <Button title="로그인" onPress={signInWithKakao} />
+    </View>
+  );
+};
 
 export default SignIn;
