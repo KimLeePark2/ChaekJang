2ff3ea521f1db33d5d95f2c5fbbc872bbbd1dc82
wogha95 <wogha95@naver.com>
2023-05-27 17:32:17 +0900

    [Refactor] props drilling 제거

--- src/components/Book/BookList.tsx
+++ src/components/Book/BookList.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import BookItem from './BookItem';
 import { DUMMY } from './DUMMY';
-import type { IBookList } from 'src/@types/book';
+import type { IBookItem } from 'src/@types/book';
 
-type PropsType = IBookList;
+type PropsType = {
+  data?: IBookItem[];
+};
 
-const BookList: React.FC<PropsType> = ({ onPressBookDetail, data = DUMMY }) => {
+const BookList: React.FC<PropsType> = ({ data = DUMMY }) => {
   return (
     <View>
       <FlatList
         data={data}
-        renderItem={({ item }) => (
-          <BookItem {...item} onPressBookDetail={onPressBookDetail} />
-        )}
+        renderItem={({ item }) => <BookItem {...item} />}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
         keyExtractor={item => item.id.toString()}
       />
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import BookList from 'src/components/Book/BookList';
 import { CompositeScreenProps } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import NewBookButton from '@components/NewBookButton';
 
 type PropsType = CompositeScreenProps<
   NativeStackScreenProps<HomeStackParamsType, 'Home'>,
@@ -11,45 +12,34 @@ type PropsType = CompositeScreenProps<
 >;
 
 const Home: React.FC<PropsType> = ({ navigation }) => {
-  const onPressBookDetail = (id: number) => {
-    navigation.navigate('BookDetail', { id });
-  };
-
   const onPressSignIn = () => {
     navigation.navigate('SignIn');
   };
 
-  const onPressNewBook = () => {
-    navigation.push('NewBook');
-  };
   return (
     <SafeAreaView edges={['bottom']}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          height: 30,
-        }}
-      >
-        <Pressable onPress={() => onPressBookDetail(1)}>
-          <Text style={{ color: 'blue' }}>(detail page)</Text>
-        </Pressable>
+      <View style={styles.container}>
         <Pressable onPress={onPressSignIn}>
-          <Text style={{ color: 'blue' }}>(sign-in page)</Text>
-        </Pressable>
-        <Pressable onPress={onPressNewBook}>
-          <Text style={{ color: 'blue' }}>(new-book page)</Text>
+          <Text style={styles.temporaryText}>(sign-in page)</Text>
         </Pressable>
       </View>
       <View>
-        <BookList onPressBookDetail={onPressBookDetail} />
+        <BookList />
       </View>
+      <NewBookButton />
     </SafeAreaView>
   );
 };
 
-// const styles = StyleSheet.create({
-//   block: {},
-// });
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    height: 30,
+  },
+  temporaryText: {
+    color: 'blue',
+  },
+});
 
 export default Home;
