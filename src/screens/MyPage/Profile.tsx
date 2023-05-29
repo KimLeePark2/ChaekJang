295dbfb797463c0fa041467fae28dd9ae617e05c
suu <suu0321@naver.com>
2023-05-29 22:18:56 +0900

    [Feat] 마이페이지 UI 수정

--- src/screens/MyPage/Profile.tsx
+++ src/screens/MyPage/Profile.tsx
@@ -1,16 +1,66 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile hi</Text>
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <View style={styles.nicknameContainer}>
+          <Text style={styles.subtitle}>안녕하세요!</Text>
+          <Text style={styles.nickname}>닉네임님</Text>
+        </View>
+        <Pressable>
+          <Text>내 프로필 보기</Text>
+        </Pressable>
+      </View>
+      <View style={styles.navigationContainer}>
+        <Pressable style={styles.navigation}>
+          <Text style={styles.navigationTitle}>판매서적</Text>
+        </Pressable>
+        <Pressable style={styles.navigation}>
+          <Text style={styles.navigationTitle}>관심목록</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  block: {},
+  container: {
+    flex: 1,
+    gap: 10,
+    backgroundColor: '#f1f1f1',
+  },
+  profileContainer: {
+    gap: 10,
+    padding: 30,
+    backgroundColor: '#fff',
+  },
+  nicknameContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 6,
+  },
+  subtitle: {
+    fontSize: 22,
+    color: '#403321',
+  },
+  nickname: {
+    fontSize: 22,
+    fontWeight: '900',
+    color: '#403321',
+  },
+  navigationContainer: {
+    flex: 1,
+    gap: 20,
+    padding: 30,
+    backgroundColor: '#fff',
+  },
+  navigation: {},
+  navigationTitle: {
+    fontSize: 18,
+    color: '#000',
+  },
 });
 
 export default Profile;
