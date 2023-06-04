f32a9a484dad4e753d8e998daac990e54c4be739
wogha95 <wogha95@naver.com>
2023-06-04 22:28:59 +0900

    [Feat] New Book Button animation

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,44 +1,31 @@
-import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { CompositeScreenProps } from '@react-navigation/native';
 import BookList from '@components/Book/BookList';
 import NewBookButton from '@components/Book/NewBookButton';
 
-type PropsType = CompositeScreenProps<
-  NativeStackScreenProps<HomeStackParamsType, 'Home'>,
-  NativeStackScreenProps<RootStackParamsType>
->;
+const Home = () => {
+  const [hidden, setHidden] = useState(false);
 
-const Home: React.FC<PropsType> = ({ navigation }) => {
-  const onPressSignIn = () => {
-    navigation.navigate('SignIn');
+  const onScrolledToBottom = (isBottom: boolean) => {
+    if (hidden !== isBottom) {
+      setHidden(isBottom);
+    }
   };
 
   return (
-    <SafeAreaView edges={['bottom']}>
-      <View style={styles.container}>
-        <Pressable onPress={onPressSignIn}>
-          <Text style={styles.temporaryText}>(sign-in page)</Text>
-        </Pressable>
-      </View>
+    <SafeAreaView edges={['bottom']} style={styles.safeAreaViewContainer}>
       <View>
-        <BookList />
+        <BookList onScrolledToBottom={onScrolledToBottom} />
       </View>
-      <NewBookButton />
+      <NewBookButton hidden={hidden} />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    height: 30,
-  },
-  temporaryText: {
-    color: 'blue',
+  safeAreaViewContainer: {
+    flex: 1,
   },
 });
 
