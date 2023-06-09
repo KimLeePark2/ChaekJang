763927d5a831dcb183adb8593f9fd271dd09ce74
lumpenop <swk5373@gmail.com>
2023-06-09 19:31:18 +0900

    [Feat] 최근 검색어 AsyncStorage 에 저장하도록 수정

--- src/screens/Search/Search.tsx
+++ src/screens/Search/Search.tsx
@@ -1,8 +1,8 @@
 import BookList from '@components/Book/BookList';
 import MainHeader from '@components/Header/MainHeader';
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import React, { useState } from 'react';
+import { colors } from '@styles/colors';
+
+import React from 'react';
 import {
   View,
   Text,
@@ -11,76 +11,27 @@ import {
   ScrollView,
 } from 'react-native';
 import XIcon from 'src/assets/svgs/x.svg';
-import { DUMMY } from '@components/Book/DUMMY';
-import NotFound from '@screens/Search/NotFound';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const SEARCH_WORDS = ['갤럭시', '아이폰', '맥북'];
 const Search = () => {
-  const navigation =
-    useNavigation<
-      NativeStackNavigationProp<SearchStackParamsType & RootStackParamsType>
-    >();
   const [inputValue, setInputValue] = React.useState('');
   const [searchValue, setSearchValue] = React.useState('');
-  const [searchWords, setSearchWords] = React.useState(SEARCH_WORDS);
-  const [replaceInputValue, setReplaceInputValue] = React.useState(
-    inputValue?.replace(/ /g, ''),
-  );
-  const [bookListData, setBookListData] = useState(DUMMY);
-  React.useEffect(() => {
-    const replaceSearchValue = searchValue?.replace(/ /g, '');
-    if (replaceSearchValue) {
-      setBookListData(prev => {
-        console.log(replaceSearchValue.length);
-        return prev.filter(item =>
-          item.title.replace(/ /g, '').includes(replaceSearchValue),
-        );
-      });
-    } else {
-      setBookListData(DUMMY);
-    }
-  }, [searchValue]);
-
-  const resetSearchWords = () => {
-    setSearchWords([]);
-  };
-
-  React.useEffect(() => {
-    setReplaceInputValue(inputValue?.replace(/ /g, ''));
-  }, [inputValue]);
+  const [words, setWords] = React.useState<string[]>([]);
 
   const onSubmit = () => {
-    // navigation.navigate('NotFound', { inputValue });
-    if (!replaceInputValue) {
-      return;
-    }
-
-    if (!searchWords.some(item => item === inputValue)) {
-      setSearchWords(prev => {
-        return [inputValue.trim(), ...prev];
-      });
-    }
-    setSearchValue(replaceInputValue);
+    setSearchValue(inputValue);
+    setWords(prev => {
+      const newWords = [inputValue, ...prev];
+      _storeData(newWords);
+      return newWords;
+    });
   };
-
   const _defaultPage = () => {
     return (
       <View style={{ flex: 1, gap: 8 }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            width: '95%',
-          }}
-        >
-          <Text style={{ fontWeight: '700', fontSize: 16 }}>최근 검색어</Text>
-          <TouchableOpacity
-            onPress={resetSearchWords}
-            style={{ justifyContent: 'flex-end' }}
-          >
-            <Text style={{ color: 'grey', fontSize: 12 }}>검색기록 삭제</Text>
-          </TouchableOpacity>
-        </View>
+        <Text style={{ fontWeight: '700', fontSize: 14, color: colors.black }}>
+          최근 검색어
+        </Text>
         <ScrollView
           contentContainerStyle={{
             flexDirection: 'row',
@@ -89,10 +40,9 @@ const Search = () => {
             justifyContent: 'space-between',
             alignItems: 'center',
             paddingHorizontal: 20,
-            marginTop: 10,
           }}
         >
-          {searchWords.map((item, index) => {
+          {words.map((item, index) => {
             return (
               <View
                 key={index}
@@ -113,17 +63,17 @@ const Search = () => {
                     setInputValue(item);
                   }}
                 >
-                  <Text style={{ width: 100 }}>{item}</Text>
+                  <Text style={{ color: colors.black }}>{item}</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                   onPress={() => {
-                    const newWords = searchWords.filter((_, idx) => {
+                    const newWords = words.filter((_, idx) => {
                       return idx !== index;
                     });
-                    setSearchWords(newWords);
+                    setWords(newWords);
                   }}
                 >
-                  <XIcon style={{ color: '#48BA95' }} />
+                  <XIcon style={{ color: '#aaa' }} width={14} />
                 </TouchableOpacity>
               </View>
             );
@@ -133,71 +83,96 @@ const Search = () => {
     );
   };
   const _searchPage = () => {
-    if (bookListData.length === 0) {
-      return <NotFound />;
-    }
     return (
       <View style={{ flex: 1 }}>
-        <BookList data={bookListData} />
+        <BookList />
       </View>
     );
+  };
+
+  React.useEffect(() => {
+    _retrieveData().then(item => setWords(item));
+  }, []);
+
+  const _storeData = async (newWords: string[]) => {
+    try {
+      await AsyncStorage.setItem('words', JSON.stringify(newWords));
+    } catch (error) {
+      console.log(error);
+      // Error saving data
+    }
+  };
+  const _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('words');
+      if (value !== null) {
+        // We have data!!
+        console.log(JSON.parse(value));
+        return JSON.parse(value);
+      }
+    } catch (error) {
+      console.log(error);
+      // Error retrieving data
+    }
   };
 
   return (
     <View style={{ flex: 1 }}>
       <MainHeader>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            width: '95%',
-            gap: 4,
-          }}
-        >
-          <TextInput
-            value={inputValue}
-            onChangeText={setInputValue}
-            placeholder="검색어를 입력하세요"
-            placeholderTextColor={'#888'}
+        <View>
+          <View
             style={{
-              borderRadius: 8,
-              backgroundColor: '#ececec',
-              width: '86%',
-              paddingHorizontal: 8,
-              paddingVertical: 10,
-              fontSize: 12,
-            }}
-          />
-          <View>
-            {inputValue && (
-              <TouchableOpacity
-                onPress={() => {
-                  setInputValue('');
-                  setSearchValue('');
-                }}
-              >
-                <XIcon
-                  style={{
-                    color: '#48BA95',
-                    right: 10,
-                    bottom: -12,
-                    position: 'absolute',
-                  }}
-                />
-              </TouchableOpacity>
-            )}
-          </View>
-          <TouchableOpacity
-            onPress={onSubmit}
-            style={{
-              backgroundColor: '#48BA95',
-              borderRadius: 4,
-              padding: 8,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              width: '95%',
             }}
           >
-            <Text style={{ color: 'white', fontWeight: 'bold' }}>검색</Text>
-          </TouchableOpacity>
+            <TextInput
+              value={inputValue}
+              onChangeText={setInputValue}
+              placeholder="검색어를 입력하세요"
+              placeholderTextColor="#888"
+              style={{
+                borderRadius: 8,
+                backgroundColor: '#ececec',
+                width: '86%',
+                paddingHorizontal: 8,
+                paddingVertical: 10,
+                fontSize: 12,
+              }}
+            />
+            <View>
+              {inputValue && (
+                <TouchableOpacity
+                  onPress={() => {
+                    setInputValue('');
+                    setSearchValue('');
+                  }}
+                >
+                  <XIcon
+                    style={{
+                      color: colors.lightGrey,
+                      right: 10,
+                      bottom: -12,
+                      position: 'absolute',
+                    }}
+                    width={20}
+                  />
+                </TouchableOpacity>
+              )}
+            </View>
+            <TouchableOpacity
+              onPress={onSubmit}
+              style={{
+                backgroundColor: '#48BA95',
+                borderRadius: 4,
+                padding: 8,
+              }}
+            >
+              <Text style={{ color: 'white', fontWeight: 'bold' }}>검색</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </MainHeader>
       <View style={{ paddingHorizontal: 14, paddingVertical: 10, flex: 1 }}>
